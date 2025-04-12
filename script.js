adcf8f5964c8168a22139f590570434f98dd2d8f
entoni-coder <entoni659@gmail.com>
2025-04-12 15:27:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ const loseSound = document.getElementById("loseSound");
 const wheelSections = [
   { text: "10", value: 10, color: "#FF6384", weight: 40 },
   { text: "20", value: 20, color: "#36A2EB", weight: 30 },
-  { text: "50", value: 50, color: "#4BC0C0", weight: 15 },
-  { text: "100", value: 100, color: "#9966FF", weight: 7 },
-  { text: "150", value: 150, color: "#FF9F40", weight: 5 },
-  { text: "200", value: 200, color: "#8AC24A", weight: 2 },
-  { text: "500", value: 500, color: "#FFCE56", weight: 1 },
+  { text: "30", value: 30, color: "#FFCE56", weight: 15 },
+  { text: "50", value: 50, color: "#4BC0C0", weight: 7 },
+  { text: "100", value: 100, color: "#9966FF", weight: 5 },
+  { text: "150", value: 150, color: "#FF9F40", weight: 2 },
+  { text: "200", value: 200, color: "#8AC24A", weight: 1 },
   { text: "You Lost", value: 0, color: "#F44336", weight: 10 }
 ];
 
@@ -52,24 +52,6 @@ function drawWheel() {
     ctx.fillText(wheelSections[i].text, radius - 10, 10);
     ctx.restore();
   }
-
-  // Disegna la freccia in basso
-  drawArrowBottom();
-}
-
-function drawArrowBottom() {
-  const centerX = canvas.width / 2;
-  const centerY = canvas.height;
-  const arrowHeight = 20;
-  const arrowWidth = 30;
-
-  ctx.beginPath();
-  ctx.moveTo(centerX - arrowWidth / 2, centerY - 5); // base sinistra
-  ctx.lineTo(centerX + arrowWidth / 2, centerY - 5); // base destra
-  ctx.lineTo(centerX, centerY - arrowHeight - 5); // punta verso l'alto
-  ctx.closePath();
-  ctx.fillStyle = "#000";
-  ctx.fill();
 }
 
 function spinWheel() {
@@ -82,7 +64,7 @@ function spinWheel() {
   const arc = 2 * Math.PI / numSections;
 
   // Ruota per almeno 5 giri + si ferma sul settore selezionato
-  const targetAngle = (2 * Math.PI * 5) + (3 * Math.PI / 2 - selectedIndex * arc - arc / 2);
+  const targetAngle = (2 * Math.PI * 5) + (Math.PI / 2 - selectedIndex * arc - arc / 2);
   let spinTime = 4000;
   let start = null;
 
@@ -130,4 +112,8 @@ function showResult(index) {
     loseSound.play();
   }
 }
+
+// Inizializza
+drawWheel();
+spinBtn.addEventListener("click", spinWheel);
 
